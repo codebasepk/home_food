c4c947fb6b61b746224fca6d7b31a11a1d90feee
hsunain hussi <husnain446@gmail.com>
2017-08-01 14:46:56 +0500

    revamp app

--- app/src/main/java/com/byteshaft/homemade/adapters/KitchenListAdapters.java
+++ app/src/main/java/com/byteshaft/homemade/adapters/KitchenListAdapters.java
@@ -38,6 +38,16 @@ public class KitchenListAdapters extends BaseAdapter{
             viewHolder.mKitchenAllDishes = (TextView) convertView.findViewById(R.id.all_dishes_number);
             viewHolder.mDishImage = (CircleImageView) convertView.findViewById(R.id.kitchen_image);
             viewHolder.mKitchenLocation = (TextView) convertView.findViewById(R.id.address);
+
+            viewHolder.mOpeningTime = (TextView) convertView.findViewById(R.id.opening_time);
+
+            viewHolder.mClosingTime = (TextView) convertView.findViewById(R.id.closing_time);
+
+            viewHolder.mOrderTime = (TextView) convertView.findViewById(R.id.order_time);
+
+            viewHolder.mWorkingDays = (TextView) convertView.findViewById(R.id.working_days);
+
+            viewHolder.mDeliverable = (TextView) convertView.findViewById(R.id.delivery_status);
             convertView.setTag(viewHolder);
 
         } else {
@@ -46,6 +56,11 @@ public class KitchenListAdapters extends BaseAdapter{
             viewHolder.mPhoneNumber.setText(kitchenList.getKitchenPhoneNumber());
             viewHolder.mKitchenAllDishes.setText(kitchenList.getKitchenAllDishes());
             viewHolder.mKitchenLocation.setText(kitchenList.getKitchenLocation());
+            viewHolder.mOpeningTime.setText(kitchenList.getOpeningTime());
+            viewHolder.mClosingTime.setText(kitchenList.getClosingTime());
+            viewHolder.mOrderTime.setText(kitchenList.getOrderTime());
+            viewHolder.mDeliverable.setText(String.valueOf(kitchenList.isDeliverable()));
+            viewHolder.mWorkingDays.setText(kitchenList.getWorkingDays());
             viewHolder.mDishImage.setImageBitmap(kitchenList.getKitchenImage());
         }
 
@@ -73,6 +88,12 @@ public class KitchenListAdapters extends BaseAdapter{
         TextView mPhoneNumber;
         TextView mKitchenAllDishes;
         TextView mKitchenLocation;
+        TextView mOpeningTime;
+        TextView mClosingTime;
+        TextView mOrderTime;
+        TextView mWorkingDays;
+        TextView mDeliverable;
         CircleImageView mDishImage;
     }
+
 }
